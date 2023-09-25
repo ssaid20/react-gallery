e7090dacbd0fced743063aab8eb0571c404d9e6d
Said Said <ssaid20@icloud.com>
2023-09-24 20:15:58 -0500

    initial commit

---
+++ server/modules/pool.js
@@ -0,0 +1,19 @@
+const pg = require("pg");
+
+const pool = new pg.Pool({
+    database: 'react_gallery',
+    host: 'localhost',
+    port: 5432,
+    
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the PostgreSQL database.');
+});
+
+pool.on('remove', () => {
+    console.log('Removed client from pool.');
+    process.exit();
+});
+
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const gallery = require('./routes/gallery.router.js');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for axios requests
-app.use(express.static('build'));
+app.use(express.json());
+ // needed for axios requests
+ app.use(bodyParser.json());
+
+const galleryRouter = require("./routes/gallery.router.js");
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/gallery', gallery);
 
+app.use(`/gallery`, galleryRouter);
 /** ---------- START SERVER ---------- **/
-app.listen(PORT,  () => {
-    console.log('Listening on port: ', PORT);
+app.listen(PORT, () => {
+  console.log("Listening on port: ", PORT);
 });
