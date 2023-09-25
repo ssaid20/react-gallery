6b386d3ed0b89776974034a491d907bea874db7b
Said Said <ssaid20@icloud.com>
2023-09-24 21:48:11 -0500

    added incremetLikes function to handle put route

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,2 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './GalleryItem.css';
 
+function GalleryItem({ item, fetchGalleryItems }) {
+    const [isDescriptionShown, setIsDescriptionShown] = useState(false);
+
+    const toggleDescription = () => {
+        setIsDescriptionShown(!isDescriptionShown);
+    };
+
+    const incrementLikes = () => {
+        axios.put(`/gallery/like/${item.id}`)
+        .then(response => {
+            fetchGalleryItems();  // refresh likes count
+        })
+        .catch(err => {
+            alert("Error incrementing likes");
+            console.log(err);
+        });
+    };
+
+    
+
+    return (
+        <div className="gallery-item">
+            <div className={`card ${isDescriptionShown ? 'flipped' : ''}`} onClick={toggleDescription}>
+                {!isDescriptionShown ? (
+                    <img src={item.path} alt={item.description} />
+                ) : (
+                    <div className="card-description">
+                        {item.description}
+                        <button onClick={deleteItem}>Delete</button>
+                    </div>
+                )}
+            </div>
+            <div className="card-actions">
+                <button onClick={incrementLikes}>Like</button>
+                <p>{item.likes} likes</p>
+            </div>
+        </div>
+    );
+}
+
+export default GalleryItem;
 
