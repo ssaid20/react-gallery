d5576134fc7beaa68998a49c812d6327fddacad4
Said Said <ssaid20@icloud.com>
2023-09-24 21:28:23 -0500

    added galleryList  component and looped through items in gallery

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,2 +1,18 @@
+import React from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem';
+import './GalleryList.css';
 
+
+function GalleryList({ galleryItems, fetchGalleryItems }) {
+    return (
+        <div className="gallery-list">
+            <h2 className="gallery-title">Gallery</h2>
+            {galleryItems.map(item => (
+                <GalleryItem key={item.id} item={item} fetchGalleryItems={fetchGalleryItems} />
+            ))}
+        </div>
+    );
+}
+
+export default GalleryList;
 
