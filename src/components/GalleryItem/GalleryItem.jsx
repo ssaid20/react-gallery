3fbbb29e38a669212bbe40db358c301b493e9999
Said Said <ssaid20@icloud.com>
2023-09-24 22:43:25 -0500

    updated GalleryItem

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
-import GalleryItem from '../GalleryItem/GalleryItem';
-import './GalleryList.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './GalleryItem.css';
 
+function GalleryItem({ item, fetchGalleryItems }) {
+    const [isDescriptionShown, setIsDescriptionShown] = useState(false);
 
-function GalleryList({ galleryItems, fetchGalleryItems }) {
+    const toggleDescription = () => {
+        setIsDescriptionShown(!isDescriptionShown);
+    };
+
+    const incrementLikes = () => {
+        axios.put(`/gallery/like/${item.id}`)
+        .then(response => {
+            fetchGalleryItems();  // refresh likes count
+        })
+        .catch(err => {
+            alert("Error incrementing likes");
+            console.log(err);
+        });
+    };
+
+    const deleteItem = () => {
+        axios.delete(`/gallery/${item.id}`)
+        .then(response => {
+            fetchGalleryItems();  // refresh gallery
+        })
+        .catch(err => {
+            alert("Error deleting item");
+            console.log(err);
+        });
+    };
+
     return (
-        <div className="gallery-list">
-            <h2 className="gallery-title">Gallery</h2>
-            {galleryItems.map(item => (
-                <GalleryItem key={item.id} item={item} fetchGalleryItems={fetchGalleryItems} />
-            ))}
+        <div className="gallery-item">
+            <div className={`card ${isDescriptionShown ? 'flipped' : ''}`} onClick={toggleDescription}>
+                {!isDescriptionShown ? (
+                    <img src={item.path} alt={item.description} />
+                ) : (
+                    <div className="card-description">
+                        {item.description}
+                        <button onClick={deleteItem}>Delete</button>
+                    </div>
+                )}
+            </div>
+            <div className="card-actions">
+                <button onClick={incrementLikes}>Like</button>
+                <p>{item.likes} likes</p>
+            </div>
         </div>
     );
 }
 
-export default GalleryList;
+export default GalleryItem;
 
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,55 +1,17 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './GalleryItem.css';
+import React from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem';
+import './GalleryList.css';
 
-function GalleryItem({ item, fetchGalleryItems }) {
-    const [isDescriptionShown, setIsDescriptionShown] = useState(false);
 
-    const toggleDescription = () => {
-        setIsDescriptionShown(!isDescriptionShown);
-    };
-
-    const incrementLikes = () => {
-        axios.put(`/gallery/like/${item.id}`)
-        .then(response => {
-            fetchGalleryItems();  // refresh likes count
-        })
-        .catch(err => {
-            alert("Error incrementing likes");
-            console.log(err);
-        });
-    };
-
-    const deleteItem = () => {
-        axios.delete(`/gallery/${item.id}`)
-        .then(response => {
-            fetchGalleryItems();  // refresh gallery
-        })
-        .catch(err => {
-            alert("Error deleting item");
-            console.log(err);
-        });
-    };
-
+function GalleryList({ galleryItems, fetchGalleryItems }) {
     return (
-        <div className="gallery-item">
-            <div className={`card ${isDescriptionShown ? 'flipped' : ''}`} onClick={toggleDescription}>
-                {!isDescriptionShown ? (
-                    <img src={item.path} alt={item.description} />
-                ) : (
-                    <div className="card-description">
-                        {item.description}
-                        <button onClick={deleteItem}>Delete</button>
-                    </div>
-                )}
-            </div>
-            <div className="card-actions">
-                <button onClick={incrementLikes}>Like</button>
-                <p>{item.likes} likes</p>
-            </div>
+        <div className="gallery-list">
+            <h2 className="gallery-title">Gallery</h2>
+            {galleryItems.map(item => (
+                <GalleryItem key={item.id} item={item} fetchGalleryItems={fetchGalleryItems} />
+            ))}
         </div>
     );
 }
 
-export default GalleryItem;
-
+export default GalleryList;
