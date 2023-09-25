18fa6d382d13d3e85b9d9155ae80386f85d207c6
Said Said <ssaid20@icloud.com>
2023-09-24 20:56:16 -0500

    added function to fetch igmaes and rendered on app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
+import GalleryForm from '../GalleryForm/GalleryForm';
 
+function App() {
+    const [galleryItems, setGalleryItems] = useState([]);
+
+    useEffect(() => {
+        fetchGalleryItems();
+    }, []);
+
+    const fetchGalleryItems = () => {
+        axios.get('/gallery')
+            .then(response => {
+                setGalleryItems(response.data);
+            })
+            .catch(err => {
+                alert("Error fetching gallery items");
+                console.log(err);
+            });
+    };
+
+    return (
+        <div className="App">
+            <h1>My Gallery</h1>
+            <GalleryForm fetchGalleryItems={fetchGalleryItems} />
+            <GalleryList galleryItems={galleryItems} fetchGalleryItems={fetchGalleryItems} />
+        </div>
+    );
+}
+
+export default App;
 
 
 
